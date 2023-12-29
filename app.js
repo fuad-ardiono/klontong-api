ac00ac5dd5ac64df3a6ea98bccde9ddd8ceca1c1
Muhammad Fuad Ardiono <fuadardiono16@gmail.com>
2023-12-29 22:15:38 +0700

    Initial commit + dockerize

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+require("dotenv").config();
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+    res.send(`Hello World! ${JSON.stringify(process.env)}`)
+})
+
+app.listen(port, "0.0.0.0", (error) => {
+	if (error) {
+		console.error(error);
+	} else {
+		console.log(
+			"Successfully connected to the server at 'http://localhost:3000/'"
+		);
+	}
+});
---
+++ infrastructure/postgres/initdb.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE klontong_db;
+\c klontong_db;
+
+CREATE OR REPLACE FUNCTION random_between(low INT ,high INT)
+   RETURNS INT AS
+$$
+BEGIN
+   RETURN floor(random()* (high-low + 1) + low);
+END;
+$$ language 'plpgsql' STRICT;
+
+CREATE TABLE auth_user (
+	user_id BIGSERIAL PRIMARY KEY,
+	email VARCHAR (255) UNIQUE NOT NULL,
+    username VARCHAR (255) UNIQUE NOT NULL,
+    name VARCHAR (255) NOT NULL,
+    password VARCHAR (255),
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE category (
+	category_id BIGSERIAL PRIMARY KEY,
+	name VARCHAR (255) UNIQUE NOT NULL
+);
+
+CREATE TABLE product_meta (
+    product_meta_id BIGSERIAL PRIMARY KEY,
+    width INT4 NULL,
+    height INT4 NULL,
+    weight INT4 NULL,
+    length INT4 NULL,
+    image TEXT NULL,
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE
+);
+
+CREATE TABLE product (
+    product_id BIGSERIAL PRIMARY KEY,
+    price BIGINT NOT NULL CHECK (price > 0),
+    description TEXT NULL,
+    category_id BIGSERIAL,
+    product_meta_id BIGSERIAL,
+    created_at DATE NOT NULL DEFAULT CURRENT_DATE,
+    CONSTRAINT fk_category FOREIGN KEY(category_id) REFERENCES category(category_id),
+    CONSTRAINT fk_product_meta FOREIGN KEY(product_meta_id) REFERENCES product_meta(product_meta_id)
+);
